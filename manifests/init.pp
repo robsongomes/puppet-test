ba09f7c0f5a2554b515d4fa3abb51ccd0dc1ea57
robson <nosbor84@gmail.com>
2017-08-20 14:47:47 -0300

    host included on all machines

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,10 @@ class puppet_test {
   contain puppet_test::conditional
   contain puppet_test::lambda
   contain puppet_test::relationship
+  contain puppet_test::host
 
   if $facts['os']['family'] == 'RedHat' {
     contain puppet_test::user
-    contain puppet_test::host
   } else {
     contain puppet_test::ssh
   }
